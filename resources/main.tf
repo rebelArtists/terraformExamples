53fd00a69285440327a2b45080b311ccbc705075
lisasiva <lisashantisiva@gmail.com>
2022-05-26 15:23:02 -0500

    working web server cluster

--- resources/main.tf
+++ resources/main.tf
@@ -7,8 +7,72 @@ variable "server_port" {
   default = 8080
 }
 
-output "public_ip" {
-  value = "${aws_instance.example.public_ip}"
+variable "elb_port" {
+  description = "port for ELB requests"
+  default = 80
+}
+
+output "elb_dns_name" {
+  value = "${aws_elb.example.dns_name}"
+}
+
+# Declare the data source
+data "aws_availability_zones" "all" {}
+
+resource "aws_autoscaling_group" "example" {
+  launch_configuration = "${aws_launch_configuration.example.id}"
+  availability_zones = "${data.aws_availability_zones.all.names}"
+
+  load_balancers = ["${aws_elb.example.name}"]
+  health_check_type = "ELB"
+
+  min_size = 2
+  max_size = 10
+
+  tag {
+    key = "Name"
+    value = "terraform-asg-example"
+    propagate_at_launch = true
+  }
+}
+
+resource "aws_security_group" "elb" {
+  name = "terraform-example-elb"
+
+  ingress {
+    from_port = "${var.elb_port}"
+    to_port = "${var.elb_port}"
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_elb" "example" {
+  name = "terraform-asg-example"
+  availability_zones = "${data.aws_availability_zones.all.names}"
+  security_groups = ["${aws_security_group.elb.id}"]
+
+  listener {
+    lb_port = 80
+    lb_protocol = "http"
+    instance_port = "${var.server_port}"
+    instance_protocol = "http"
+  }
+
+  health_check {
+    healthy_threshold = 2
+    unhealthy_threshold = 2
+    timeout = 3
+    interval = 30
+    target = "HTTP:${var.server_port}/"
+  }
 }
 
 resource "aws_security_group" "instance" {
@@ -20,12 +84,16 @@ resource "aws_security_group" "instance" {
     protocol = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
+
+  lifecycle {
+    create_before_destroy = true
+  }
 }
 
-resource "aws_instance" "example" {
-  ami = "ami-07ebfd5b3428b6f4d"
+resource "aws_launch_configuration" "example" {
+  image_id = "ami-07ebfd5b3428b6f4d"
   instance_type = "t2.micro"
-  vpc_security_group_ids = ["${aws_security_group.instance.id}"]
+  security_groups = ["${aws_security_group.instance.id}"]
 
   user_data = <<-EOF
               #!/bin/bash
@@ -33,7 +101,8 @@ resource "aws_instance" "example" {
               nohup busybox httpd -f -p 8080 &
               EOF
 
-  tags = {
-    Name = "terraform-example"
+  lifecycle {
+    create_before_destroy = true
   }
+
 }
